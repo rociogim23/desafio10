183f331b82846093b993d249770855ffc3208585
rociogim23 <141087441+rociogim23@users.noreply.github.com>
2023-08-28 19:31:41 -0300

    subido

---
+++ product-script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const productName = document.getElementById("productName");
+    const productDescription = document.getElementById("productDescription");
+  
+    
+    //Recupera el valor almacenado en el localStorage con la clave "ProdID"
+    
+    //Busca ese producto en la matriz de productos y muestra su nombre y descripción en la página de detalles del producto.
+});
+
+
+ 
